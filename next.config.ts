7e8f7ff8064da50859f717088475a44bc2b7e316
Hafiyyan <hafiyyan94@gmail.com>
2024-12-24 22:44:47 +0700

    NextJS whitelist for local development

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	env: {
-		sakinahAPI: "https://sakinah-be.hafiyyansayy.id",
-	},
 	images: {
 		remotePatterns: [
 			{
 				protocol: "https",
 				hostname: "sakinah-be.hafiyyansayy.id",
 				port: "",
+				pathname: "/media/**",
+			},
+			{
+				protocol: "http",
+				hostname: "127.0.0.1",
+				port: "8000",
 				pathname: "/media/**",
 			},
 		],
