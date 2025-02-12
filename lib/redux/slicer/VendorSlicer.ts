15aaa7adcc77d3d87559a368a7b57d1d87b5f207
Firyal <97155805+firyalhfz@users.noreply.github.com>
2025-02-12 23:29:43 +0700

    Fix detail vendor (#22)
    Vendor Detail and its Package

--- lib/redux/slicer/VendorSlicer.ts
+++ lib/redux/slicer/VendorSlicer.ts
@@ -2,118 +2,123 @@ import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 import axios from "axios";
 
 export interface TypeVendor {
-	// [x: string]: string;
-	// title: string;
-	id: number;
-	name: string;
-	description: string;
-	about: string;
-	category: string;
-	city: string;
-	email: string;
-	facebook: string;
-	instagram: string;
-	tiktok: string;
-	youtube: string;
-	profile_image: string;
-	availability: boolean;
-	// visibilty: string;
-	visibility: "private" | "public";
-	galleries: TypeVendorGalleries[];
-	packages: TypeVendorPackages[];
-	schedules: TypeVendorSchedules[];
-	// starting_price: number;
-	[key: string]: number | string | unknown;
+  // [x: string]: string;
+  // title: string;
+  id: number;
+  name: string;
+  description: string;
+  about: string;
+  category: string;
+  city: string;
+  email: string;
+  facebook: string;
+  instagram: string;
+  tiktok: string;
+  youtube: string;
+  profile_image: string;
+  availability: boolean;
+  // visibilty: string;
+  visibility: "private" | "public";
+  galleries: TypeVendorGalleries[];
+  packages: TypeVendorPackages[];
+  schedules: TypeVendorSchedules[];
+  // starting_price: number;
+  [key: string]: number | string | unknown;
 }
 
-type TypeVendorGalleries = {
-	id: number;
-	image: string;
-};
 export type TypeVendorPackages = {
-	id: number;
-	name: string;
-	price: number;
-	description: string;
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  // first_image: string;
+  galleries: TypeVendorGalleries[];
+  terms_and_condition: string;
 };
+
+type TypeVendorGalleries = {
+  id: number;
+  image: string;
+};
+
 type TypeVendorSchedules = {
-	day: string;
-	start_time: string;
-	end_time: string;
+  day: string;
+  start_time: string;
+  end_time: string;
 };
 
 interface GenericState<T> {
-	vendor: T[];
-	loadingVendor: boolean;
-	error: string | null;
+  vendor: T[];
+  loadingVendor: boolean;
+  error: string | null;
 }
 
 const initialState: GenericState<TypeVendor> = {
-	vendor: [],
-	loadingVendor: false,
-	error: null,
+  vendor: [],
+  loadingVendor: false,
+  error: null,
 };
 
 export const fetchVendor = createAsyncThunk(
-	"master_data/vendors",
-	async (params?: {
-		basePath?: string | number; // Optional base path
-		queryParams?: Record<string, string | number | undefined>; // Query parameters
-	}): Promise<[TypeVendor]> => {
-		const { basePath, queryParams } = params || {};
+  "master_data/vendors",
+  async (params?: {
+    basePath?: string | number; // Optional base path
+    queryParams?: Record<string, string | number | undefined>; // Query parameters
+  }): Promise<[TypeVendor]> => {
+    const { basePath, queryParams } = params || {};
 
-		// Construct query parameters dynamically
-		const queryString = queryParams
-			? new URLSearchParams(
-					Object.entries(queryParams).reduce((acc, [key, value]) => {
-						if (value !== undefined) acc[key] = value.toString();
-						return acc;
-					}, {} as Record<string, string>)
-			  ).toString()
-			: "";
+    // Construct query parameters dynamically
+    const queryString = queryParams
+      ? new URLSearchParams(
+          Object.entries(queryParams).reduce((acc, [key, value]) => {
+            if (value !== undefined) acc[key] = value.toString();
+            return acc;
+          }, {} as Record<string, string>)
+        ).toString()
+      : "";
 
-		// Construct the final URL
-		const url = `${process.env.sakinahAPI}/master_data/vendors${
-			basePath ? `/${basePath}` : ""
-		}${queryString ? `?${queryString}` : ""}`;
+    // Construct the final URL
+    const url = `${process.env.sakinahAPI}/master_data/vendors${
+      basePath ? `/${basePath}` : ""
+    }${queryString ? `?${queryString}` : ""}`;
 
-		const response = await axios.get(url);
-		// console.log(basePath ? "ok" : queryString, url);
+    const response = await axios.get(url);
+    // console.log(basePath ? "ok" : queryString, url);
 
-		return basePath ? response.data : response.data.results;
-	}
+    return basePath ? response.data : response.data.results;
+  }
 );
 
 const vendorSlice = createSlice({
-	name: "vendors",
-	initialState,
-	reducers: {
-		start(state) {
-			state.loadingVendor = true;
-		},
-		vendorReducer(state, action: PayloadAction<TypeVendor[]>) {
-			state.loadingVendor = false;
-			state.vendor = [...state.vendor, ...action.payload];
-		},
-		failure(state, action: PayloadAction<string>) {
-			state.loadingVendor = false;
-			state.error = action.payload;
-		},
-	},
-	extraReducers: (builder) => {
-		builder.addCase(fetchVendor.pending, (state) => {
-			state.loadingVendor = true;
-		});
-		builder.addCase(fetchVendor.fulfilled, (state, action) => {
-			// console.log(action.payload);
-			state.vendor = action.payload;
+  name: "vendors",
+  initialState,
+  reducers: {
+    start(state) {
+      state.loadingVendor = true;
+    },
+    vendorReducer(state, action: PayloadAction<TypeVendor[]>) {
+      state.loadingVendor = false;
+      state.vendor = [...state.vendor, ...action.payload];
+    },
+    failure(state, action: PayloadAction<string>) {
+      state.loadingVendor = false;
+      state.error = action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchVendor.pending, (state) => {
+      state.loadingVendor = true;
+    });
+    builder.addCase(fetchVendor.fulfilled, (state, action) => {
+      // console.log(action.payload);
+      state.vendor = action.payload;
 
-			state.loadingVendor = false;
-		});
-		builder.addCase(fetchVendor.rejected, (state) => {
-			state.loadingVendor = false;
-		});
-	},
+      state.loadingVendor = false;
+    });
+    builder.addCase(fetchVendor.rejected, (state) => {
+      state.loadingVendor = false;
+    });
+  },
 });
 
 // Export the reducer and actions
