4672b78a166747dcb549ea51091547d3ced4bef9
fikarpakarpikir <fikar.mohammad.6.fm@gmail.com>
2025-02-12 12:24:26 +0700

    debug

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,50 +5,53 @@ import Image from "next/image";
 
 export const Footer = () => {
 	return (
-		<footer
-			role='contentinfo'
-			className='bg-primary2 text-white md:px-32 px-16 pt-3 pb-2 shadow-lg rounded-t-2xl shadow-lg'>
-			<div className='text-center pb-1 bg-white rounded-full w-24 mx-auto'></div>
-			<div className='text-white rounded-lg'>
-				<div className='relative w-32 h-32 text-center mx-auto my-5'>
-					<Image src={`/logo/PNG/primary_5.png`} alt='secondary_5' fill />
-				</div>
-				<hr />
-				<div className='flex text-xs py-3 justify-between'>
-					<div className='flex gap-8'>
-						<span>
-							<FontAwesomeIcon icon={faCopyright} /> 2024 Wedding Platform, Inc.
-						</span>
-						{/* <ul className='flex flex-wrap items-center justify-center gap-5'>
+		<div className='h-full py-28'>
+			<footer
+				role='contentinfo'
+				className='bg-primary2 text-white md:px-32 px-16 fixed bottom-0 left-0 w-full pt-3 pb-2 shadow-lg rounded-t-2xl shadow-lg'>
+				<div className='text-center pb-1 bg-white rounded-full w-24 mx-auto'></div>
+				<div className='text-white rounded-lg'>
+					<div className='relative w-32 h-32 text-center mx-auto my-5'>
+						<Image src={`/logo/PNG/primary_5.png`} alt='secondary_5' fill />
+					</div>
+					<hr />
+					<div className='flex text-xs py-3 justify-between'>
+						<div className='flex gap-8'>
+							<span>
+								<FontAwesomeIcon icon={faCopyright} /> 2024 Wedding Platform,
+								Inc.
+							</span>
+							{/* <ul className='flex flex-wrap items-center justify-center gap-5'>
 							<li>- Privacy</li>
 							<li>- Terms</li>
 						</ul> */}
-					</div>
-					<div className='flex gap-2'>
-						{/* <span>
+						</div>
+						<div className='flex gap-2'>
+							{/* <span>
 							<FontAwesomeIcon icon={faGlobe} />
 							English (US)
 						</span> */}
-						{/* <FontAwesomeIcon icon={faFacebook} />
+							{/* <FontAwesomeIcon icon={faFacebook} />
 						<FontAwesomeIcon icon={faX} /> */}
 
-						<a
-							href='https://www.instagram.com/sakinahweddingplatform/'
-							target='_blank'
-							rel='noopener noreferrer'
-							// className='btn-primary py-2 px-4 rounded-full text-xl'
-						>
-							{/* <FontAwesomeIcon
+							<a
+								href='https://www.instagram.com/sakinahweddingplatform/'
+								target='_blank'
+								rel='noopener noreferrer'
+								// className='btn-primary py-2 px-4 rounded-full text-xl'
+							>
+								{/* <FontAwesomeIcon
 									icon={faInstagramSquare}
 									className='me-2'
 									size='xl'
 								/> */}
 
-							<FontAwesomeIcon icon={faInstagram} size='xl' />
-						</a>
+								<FontAwesomeIcon icon={faInstagram} size='xl' />
+							</a>
+						</div>
 					</div>
 				</div>
-			</div>
-		</footer>
+			</footer>
+		</div>
 	);
 };
